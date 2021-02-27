46cd172ece9a81bc5546f999a033ba52a6340621
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2021-02-27 19:39:03 +0100

    Init

---
+++ src/main/java/dev/stanuch/htmleditor/HtmleditorApplication.java
@@ -0,0 +1,13 @@
+package dev.stanuch.htmleditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmleditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmleditorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/stanuch/htmleditor/HtmleditorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stanuch.htmleditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmleditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
