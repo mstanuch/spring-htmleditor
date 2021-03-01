d58ff0da77f65942c00edaf6a566255c06f5380a
Mateusz Stanuch <mateusz.stanuch@gmail.com>
2021-03-01 23:03:36 +0100

    Add lombok

--- src/main/java/dev/stanuch/htmleditor/entities/Post.java
+++ src/main/java/dev/stanuch/htmleditor/entities/Post.java
@@ -5,40 +5,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private long id;
 
     @NotEmpty(message = "Name is mandatory")
+    @Getter
+    @Setter
     private String name;
 
     @NotEmpty(message = "Content is mandatory")
+    @Getter
+    @Setter
     private String content;
-
-    public long getId() {
-        return this.id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return this.content;
-    }
 }
